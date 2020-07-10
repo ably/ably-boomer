46ab02a1782eaf081e6229fa3d2cd11651e0ce41
James Frost <james.frost@ably.com>
2020-07-10 09:30:42 +0100

    Simplify 'newAblyClient' function

--- ably/ably_client.go
+++ ably/ably_client.go
@@ -8,10 +8,5 @@ func newAblyClient(testConfig TestConfig) (*ably.RealtimeClient, error) {
 	options := ably.NewClientOptions(testConfig.ApiKey)
 	options.Environment = testConfig.Env
 
-	client, err := ably.NewRealtimeClient(options)
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
+	return ably.NewRealtimeClient(options)
 }
