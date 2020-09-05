e3bf1d125971af80423dcb2cd648362a64bd179e
Lewis Marshall <lewis@lmars.net>
2020-09-05 11:26:18 +0100

    Use log15 structured logging
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- ably/report_sub_to_locust.go
+++ ably/report_sub_to_locust.go
@@ -6,14 +6,12 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
-	"log"
 
 	"github.com/ably-forks/boomer"
 	"github.com/ably/ably-go/ably"
 )
 
 func reportSubscriptionToLocust(ctx context.Context, sub *ably.Subscription, conn *ably.Conn, errorChannel chan<- error, wg *sync.WaitGroup) {
-
 	connectionStateChannel := make(chan ably.State)
 	conn.On(connectionStateChannel)
 
@@ -23,35 +21,43 @@ func reportSubscriptionToLocust(ctx context.Context, sub *ably.Subscription, con
 		select {
 		case connState, ok := <-connectionStateChannel:
 			if !ok {
-				log.Println("Connection State Error - Channel Closed")
+				log.Warn("connection state channel closed", "id", conn.ID())
 				err := errors.New("Connection State Channel Closed")
 				errorChannel <- err
 				continue
 			}
 
-			log.Println("Connection State Change -", connState.State)
+			log.Info(
+				"connection state changed",
+				"id", conn.ID(),
+				"key", conn.Key(),
+				"state", connState.State,
+				"err", connState.Err,
+			)
 
 			if connState.State == ably.StateConnDisconnected {
 				lastDisconnectTime = millisecondTimestamp()
 			} else if connState.State == ably.StateConnConnected && lastDisconnectTime != 0 {
 				timeDisconnected := millisecondTimestamp() - lastDisconnectTime
 
+				log.Info("reporting reconnect time", "id", conn.ID(), "duration", timeDisconnected)
 				boomer.RecordSuccess("ably", "reconnect", timeDisconnected, 0)
 			}
 		case <-ctx.Done():
+			log.Info("subscriber context done", "id", conn.ID())
 			wg.Done()
 			return
 		case msg, ok := <-sub.MessageChannel():
 			if !ok {
-				log.Println("Sub Message Error - Channel Closed")
-				err := errors.New("Sub Message Channel Closed")
+				log.Warn("subscriber message channel closed", "id", conn.ID())
+				err := errors.New("Subscriber Message Channel Closed")
 				errorChannel <- err
 				continue
 			}
 
 			timePublished, err := strconv.ParseInt(msg.Name, 10, 64)
-
 			if err != nil {
+				log.Error("error parsing message name as timestamp", "id", conn.ID(), "err", err)
 				boomer.RecordFailure("ably", "subscribe", 0, err.Error())
 				break
 			}
@@ -59,6 +65,7 @@ func reportSubscriptionToLocust(ctx context.Context, sub *ably.Subscription, con
 			timeElapsed := millisecondTimestamp() - timePublished
 			bytes := len(fmt.Sprint(msg.Data))
 
+			log.Info("received message", "conn.id", conn.ID(), "size", bytes, "latency", timeElapsed)
 			boomer.RecordSuccess("ably", "subscribe", timeElapsed, int64(bytes))
 		}
 	}
--- go.mod
+++ go.mod
@@ -8,6 +8,9 @@ require (
 	github.com/ably/ably-go v1.1.6-0.20200824141310-49d7069ca624
 	github.com/aws/aws-sdk-go v1.33.17
 	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/inconshreveable/log15 v0.0.0-20200109203555-b30bc20e4fd1
+	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/zeromq/goczmq v4.1.0+incompatible // indirect
-	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
+	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
 )
